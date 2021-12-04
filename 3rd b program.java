7d26022d4e848cc1750caac0dcfdc9e12095e368
Jashwanthik-2801 <69189090+Jashwanthik-2801@users.noreply.github.com>
2021-12-04 10:52:30 +0530

    Add files via upload

---
+++ 3rd b program.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		int key = sc.nextInt();
+		String encrypted = encrypt(str,key);
+		System.out.println("Encrypted String = "+encrypted);
+		String decrypted = decrypt(str,key);
+		System.out.println("Decrypted String = "+decrypted);
+	}
+	public static String encrypt(String str,int key){
+	    String encrypted = "";
+	    for(int i = 0;i<str.length();i++){
+	        int c = str.charAt(i);
+	        if(Character.isUpperCase(c)){
+	            c = c + (key % 26);
+	            if(c > 'Z')
+	                c = c - 26;
+	        }
+	        else if(Character.isLowerCase(c)){
+	            c = c + (key % 26);
+	            if(c > 'z')
+	                c = c - 26;
+	        }
+	        encrypted += (char)c;
+	    }
+	    return encrypted;
+	    
+	}
+	public static String decrypt(String str,int key){
+	    String decrypted = "";
+	    for(int i = 0;i<str.length();i++){
+	        int c = str.charAt(i);
+	        if(Character.isUpperCase(c)){
+	            c = c - (key % 26);
+	            if(c < 'A')
+	                c = c + 26;
+	        }
+	        else if(Character.isLowerCase(c)){
+	            c = c - (key % 26);
+	            if(c < 'a')
+	                c = c + 26;
+	        }
+	        decrypted += (char)c;
+	    }
+	    return decrypted;
+    }
+}
